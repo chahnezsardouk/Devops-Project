62ca5b1aaa090922b16a7ac9493e23dc3df764e9
chahnezsardouk <chahnez.sardouk@esprit.tn>
2023-11-01 21:37:03 +0100

    grafana

--- src/main/java/tn/esprit/rh/achat/repositories/StockRepository.java
+++ src/main/java/tn/esprit/rh/achat/repositories/StockRepository.java
@@ -2,13 +2,14 @@ package tn.esprit.rh.achat.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.rh.achat.entities.Stock;
 
 import java.util.List;
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {
+public interface StockRepository extends CrudRepository<Stock, Long> {
 	
 
 	
